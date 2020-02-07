fb5a758e6f135c24dbb1eae4c0d7e231795be5cd
Ramprasad-R <ramprasad.info@gmail.com>
2020-02-07 22:46:09 +0100

    Added custom counter logic

--- messages-api.js
+++ messages-api.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const rateLimit = require("express-rate-limit");
+//const rateLimit = require("express-rate-limit");
 const app = express();
-const limiter = rateLimit({
-  max: 5
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   max: 5
+// });
+//app.use(limiter);
 app.use(express.json());
-app.post("/messages", (req, res) => {
+let counter = 0;
+const counterMiddleware = (req, res, next) => {
+  counter = counter + 1;
+  if (counter <= 5) {
+    next();
+  } else {
+    res.sendStatus(429);
+  }
+};
+app.post("/messages", counterMiddleware, (req, res) => {
   const message = req.body;
+
   if (JSON.stringify(message) !== "{}") {
     console.log(message);
     res.sendStatus(200);
