91f9b9ee20cd3096e006f982cdef61053d97ea3a
Ramprasad-R <ramprasad.info@gmail.com>
2020-02-08 18:49:33 +0100

    Added heroku Procfile and env variable for heroku

--- messages-api.js
+++ messages-api.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = process.env.PORT || process.env.MESSAGE_API_PORT;
 app.use(express.json());
 let counter = 0;
 const counterMiddleware = (req, res, next) => {
@@ -11,18 +12,25 @@ const counterMiddleware = (req, res, next) => {
     res.sendStatus(429);
   }
 };
-app.post("/messages", counterMiddleware, (req, res) => {
-  const message = req.body;
+app
+  .post("/messages", counterMiddleware, (req, res) => {
+    const message = req.body;
 
-  if (JSON.stringify(message) !== "{}") {
-    console.log(message);
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(400);
-  }
-});
-app.listen(process.env.MESSAGE_API_PORT, () => {
-  console.log(
-    `Server Started and running on port: ${process.env.MESSAGE_API_PORT}`
+    if (JSON.stringify(message) !== "{}") {
+      console.log(message);
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(400);
+    }
+  })
+  .get("*", (req, res) =>
+    res
+      .status(400)
+      .send(
+        "Bad request send request to /messages endpoint. Example http :3000/messages message=testData"
+      )
   );
+
+app.listen(port, () => {
+  console.log(`Server Started and running on port: ${port}`);
 });
--- sequelize-rest.js
+++ sequelize-rest.js
@@ -2,12 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const movieRouter = require("./movie/router");
+const port = process.env.PORT || process.env.SEQUELIZE_REST_PORT;
 app.use(cors());
 app.use(express.json());
-const movieRouter = require("./movie/router");
 app.use(movieRouter);
-app.listen(process.env.SEQUELIZE_REST_PORT, () => {
-  console.log(
-    `Server Started and running on port: ${process.env.SEQUELIZE_REST_PORT}`
-  );
+app.listen(port, () => {
+  console.log(`Server Started and running on port: ${port}`);
 });
